5a40e879e61f1ac2a14e04cf23c7f9068c9734b6
Ulvi <ulvijalil7@gmail.com>
2025-07-14 19:31:01 +0400

    Client Updated SearchForm

--- client/src/components/home/SearchForm/SearchForm.tsx
+++ client/src/components/home/SearchForm/SearchForm.tsx
@@ -28,13 +28,13 @@ const SearchForm = () => {
     return (
         <div className="py-30 secondary-font">
             <div className="flex bg-[#292929] rounded-[6px] mx-auto w-[460px] py-3 justify-center items-center">
-                <div className="flex items-center gap-1">
-                    <div className="bg-[#e51515] rounded-[6px] px-4 py-2.5">
+                <div className="flex flex-col md:flex-row items-center gap-2">
+                    <div className="bg-[#e51515] rounded-[6px] px-4 py-2.5 w-full md:w-auto text-center">
                         <h2 className="text-[#fff] font-semibold text-[12px]">
                             SEARCH BY YEAR, MAKE, MODEL
                         </h2>
                     </div>
-                    <div className="rounded-[6px] px-4 py-2.5">
+                    <div className="rounded-[6px] px-4 py-2.5 w-full md:w-auto text-center">
                         <h2 className="text-[#fff] font-semibold text-[12px]">
                             MAKE, MODEL, YEAR, TYPE
                         </h2>
@@ -42,19 +42,20 @@ const SearchForm = () => {
                 </div>
             </div>
 
-            <div className="bg-[#292929] mt-5 flex py-5 justify-center mx-auto items-center w-[900px]">
-                <div className="flex gap-4 justify-center items-center mx-auto">
+            <div className="bg-[#292929] mt-5 flex justify-center mx-auto items-center w-full max-w-[900px]">
+                <div className="bg-[#292929]  flex flex-col md:flex-row py-5 justify-center mx-auto items-center w-full max-w-[900px] gap-4">
                     <CarMakeDropdown onSelect={handleMakeSelect} />
                     <CarModelDropdown models={models} onSelect={handleModelSelect} />
                     <CarYearDropdown onSelect={handleYearSelect} />
 
                     <button
                         onClick={handleSearch}
-                        className="bg-[#e51515] text-white px-4 py-2 w-50 rounded"
+                        className="bg-[#e51515] text-white px-4 py-2 w-[200px] rounded"
                     >
                         Search
                     </button>
                 </div>
+
             </div>
         </div>
     );
