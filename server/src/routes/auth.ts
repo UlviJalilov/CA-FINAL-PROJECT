df0e280bd627ea090f1e74aeec1caf619094c78d
Ulvi <ulvijalil7@gmail.com>
2025-07-16 16:46:47 +0400

    Server: Created Auth

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,95 @@
+// routes/auth.js
+
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/UserSchema";
+
+const router = express.Router();
+
+// Register route
+router.post("/register", async (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        
+        const newUser = new User({ firstName, lastName, email, password: hashedPassword });
+        await newUser.save();
+
+
+        const jwtSecret = process.env.JWT_SECRET;
+        if (!jwtSecret) {
+            throw new Error("JWT_SECRET is not defined in .env file");
+        }
+
+        const token = jwt.sign(
+            { id: newUser._id },
+            process.env.JWT_SECRET!,
+            { expiresIn: "7d" }
+        );
+
+        res.status(201).json({
+            message: "User registered successfully",
+            token,
+            user: {
+                id: newUser._id,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                email: newUser.email,
+            },
+        });
+    } catch (err: unknown) {
+        const error = err as Error;
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+});
+
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+     
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+     
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET!,
+            { expiresIn: "7d" }
+        );
+
+      
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+        });
+    } catch (err: unknown) {
+        const error = err as Error;
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+});
+
+
+export default router;
