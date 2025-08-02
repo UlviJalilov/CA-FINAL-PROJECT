f3aa9567f16ed27c29ea10862ca6d99fd1e285d9
Ulvi <ulvijalil7@gmail.com>
2025-08-02 20:43:07 +0400

    Client: Created CartBanner

---
+++ client/src/components/shared/CartBanner/CartBanner.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from "next/image"
+import Cartcrumb from '../Cartcrumb/Cartcrumb'
+import Navbar from '../Navbar/Navbar'
+
+
+const CartBanner = () => {
+    return (
+        <div>
+            <div>
+                <div className="relative w-full h-[500px]">
+                    <Image
+                        src="https://aero-theme.myshopify.com/cdn/shop/t/39/assets/collection_top.jpg?v=117586586586651228491725589094"
+                        alt="Register Banner Img"
+                        layout="fill"
+                        objectFit="cover"
+                        quality={100}
+                        unoptimized={true}
+                    />
+                    <div className='absolute inset-0 flex justify-center flex-col gap-5 items-center'>
+                        <h1 className='text-white text-center text-5xl font-medium primary-font'>YOUR SHOPPING CART</h1>
+                        <Cartcrumb />
+
+                    </div>
+
+                </div>
+              <Navbar />
+            </div>
+        </div>
+    )
+}
+
+export default CartBanner
