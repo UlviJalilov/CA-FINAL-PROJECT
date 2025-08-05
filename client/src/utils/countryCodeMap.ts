b8b5868ee98b26aac882b6a232aed5e4a8991b5d
Ulvi <ulvijalil7@gmail.com>
2025-08-06 01:54:05 +0400

    feat: add country code map utility

---
+++ client/src/utils/countryCodeMap.ts
@@ -0,0 +1,10 @@
+export const countryCodeMap: { [key: string]: string } = {
+  Azerbaijan: "AZ",
+  Turkey: "TR",
+  "United States": "US",
+  GB: "GB",
+  Germany: "DE",
+  Almaniya: "DE",
+  Russia: "RU",
+  Uzbekistan: "UZ",
+};
