d294dffe9af0e517397566db602b7e78213dc0a9
Ulvi <ulvijalil7@gmail.com>
2025-08-12 20:05:31 +0400

    Home: change on layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ReactNode, useState } from "react";
 import { CartProvider } from "@/context/CartContext";
 import AdBanner from "@/components/home/AdBanner/AdBanner";
 import Footer from "@/components/shared/Footer/Footer";
-import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
 import "react-toastify/dist/ReactToastify.css";
 import { WishlistProvider } from "@/context/WishlistContext";
 
@@ -26,18 +26,24 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               <AdBanner />
               <main>{children}</main>
               <Footer />
-              <ToastContainer
-                position="top-right"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-              />
+               <Footer />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration: 3000,
+                style: {
+                  borderRadius: '15px',
+                  background: '#e51515',
+                  color: '#fff',
+                  fontSize: '15px',
+                  padding: '15px 16px',
+                },
+                iconTheme: {
+                  primary: '#e51515',
+                  secondary: '#fff',
+                },
+              }}
+            />
             </WishlistProvider>
           </CartProvider>
         </QueryClientProvider>
