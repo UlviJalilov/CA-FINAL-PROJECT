f4885e74b1f58597665a3075b90b3cf39b65333b
Ulvi <ulvijalil7@gmail.com>
2025-08-13 22:59:40 +0400

    feat: create Shop page

---
+++ client/src/app/shop/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ShopBanner from '@/components/shop/ShopBanner/ShopBanner';
+import CategorySection from '@/components/shop/CategorySection/CategorySection';
+import ShopGrid from '@/components/shop/ShopGrid/ShopGrid';
+import { FeaturedProduct } from "@/types/FeaturedProduct";
+
+
+
+
+export default async function ShopPage() {
+    const res = await fetch("http://localhost:3001/api/featured-products", {
+        cache: "no-store",
+    });
+
+    const products: FeaturedProduct[] = await res.json();
+
+    const images: string[] = products.map((p) => p.image);
+
+    return (
+        <div className='bg-white'>
+            <ShopBanner />
+            <CategorySection images={images} />
+            <ShopGrid products={products} />
+        </div>
+    );
+}
