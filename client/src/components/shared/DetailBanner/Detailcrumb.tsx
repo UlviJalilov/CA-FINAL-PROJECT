6b2b8e8fa153273289a46e12fe83d19adfc5fc8c
Ulvi <ulvijalil7@gmail.com>
2025-08-15 13:18:07 +0400

    - Updated Detailcrumb.tsx for mobile-friendly centered breadcrumb

--- client/src/components/shared/DetailBanner/Detailcrumb.tsx
+++ client/src/components/shared/DetailBanner/Detailcrumb.tsx
@@ -4,9 +4,12 @@ import Link from "next/link"
 const Detailcrumb = ({ title }: { title: string }) => {
   return (
     <div>
-      <nav className="text-sm text-gray-500 mb-4" aria-label="Breadcrumb">
-        <ol className="list-reset flex">
-          <li className="flex text-white gap-4 font-medium text-[16px] items-center justify-center">
+      <nav
+        className="text-sm text-gray-500 mb-4"
+        aria-label="Breadcrumb"
+      >
+        <ol className="list-reset flex flex-wrap justify-center text-center">
+          <li className="flex flex-wrap gap-2 sm:gap-4 text-white font-medium text-[16px] items-center justify-center">
             <Link className="hover:text-[#e51515]" href="/">
               Home
             </Link>
@@ -15,6 +18,7 @@ const Detailcrumb = ({ title }: { title: string }) => {
           </li>
         </ol>
       </nav>
+
     </div>
   )
 }
